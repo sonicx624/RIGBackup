a3b730803c4b64dbca8c828bf8396098d23ca552
root <sonicx624@gmail.com>
2019-07-10 23:55:55 +0000

    commit

--- sysbenchFileIO.bash
+++ sysbenchFileIO.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 MB="MB"
-for (( i=128; i<=128; i=$i*2 )) do
-	for (( j=1; j<=1; j++ )) do
+for (( i=128; i<=8192; i=$i*2 )) do
+	for (( j=1; j<=10; j++ )) do
 		sysbench fileio --file-total-size="$i$MB" prepare >> /dev/null
 		if [ "$j" != "1" ]; then
 			sysbench fileio --file-total-size="$i$MB" --file-test-mode=rndrw --time=300 --max-requests=0 run >> "FileIO$i$MB.test"
